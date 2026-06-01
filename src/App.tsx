6690d9bc64236717866ab864558656c4224614d7
Alejandro <aandradez094@gmail.com>
2026-06-01 17:16:37 -0500

    Clase 01-06-2026 avance borrador proyecto 2

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import './App.css'
+import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI.tsx';
+import AlertUI from './components/AlertUI.tsx';
+
+function App() {  
+  return (
+    <Grid container spacing={5} justifyContent="center" alignItems="center">
+
+
+         {/* Encabezado */}
+         <Grid size={{ xs: 12, md: 12 }}><HeaderUI/> </Grid>
+
+         {/* Alertas */}
+         <Grid size={{ xs: 12, md: 12 }} container justifyContent="right" alignItems="center">
+
+             <AlertUI description="No se preveen lluvias"/></Grid>
+
+         {/* Selector */}
+         <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+
+         {/* Indicadores */}
+         <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+
+         {/* Gráfico */}
+         <Grid size={{ xs: 12, md: 6 }}sx={{ display: { xs: "none", md: "block"} }} >
+            Elemento: Gráfico
+         </Grid>
+
+         {/* Tabla */}
+         <Grid size={{ xs: 12, md: 6 }}sx={{ display: { xs: "none", md: "block" } }}>
+            Elemento: Tabla
+          </Grid>
+
+         {/* Información adicional */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+
+      </Grid>
+      )
+}
+
+export default App
